839d01ceeb25faaeb03e5560fcf935c001403642
adolf <135438640+bars1k1337@users.noreply.github.com>
2023-07-01 16:21:45 +0300

    add ball speed and walls collision

--- ping_pong.py
+++ ping_pong.py
@@ -40,10 +40,20 @@ clock = time.Clock()
 
 FPS = 60
 game = True
+finish = False
+speed_x = 3
+speed_y = 3
+
 while game:
     for e in event.get():
             if e.type == QUIT:
                   game = false
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
 
+    if ball.rect.y > width - 50 or ball.rect.y < 0:
+        speed_y *= -1
+     ball.update()   
     display.update()
     clock.tick(FPS)
